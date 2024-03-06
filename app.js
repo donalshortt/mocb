1a1daae99729018f73ecae82ead61bd3d6145298
donal shortt <donalshortt@donals.eu>
2024-03-06 13:28:46 +0100

    refactor(server): Refactor monolithic structure
    Everything is handled in a single file, this refactor aims to enforce a seperation of concerns

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const gameRoutes = require('./api/routes/gameRoutes');
+const modifierRoutes = require('./api/routes/modifierRoutes');
+
+const app = express();
+const port = process.env.PORT || 3080;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../front/build')));
+app.use(cors());
+
+// Initialize data directory
+require('./api/utils/fileUtils').initDataDir();
+
+// Routes
+app.use('/api', gameRoutes);
+app.use('/api', modifierRoutes);
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
